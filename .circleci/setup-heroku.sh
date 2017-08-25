0efc3c5c868888fa9e3addbdd286abb35bc88bcb
rafaelrabetti <rafael.rabetti@gmail.com>
2017-08-25 16:21:27 -0300

    rs

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+  git remote add heroku https://git.heroku.com/gcs01-rafaelrabetti-circle.git
   wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
   mkdir -p /usr/local/lib /usr/local/bin
   tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-  git remote add heroku https://git.heroku.com/gcs01-rafaelrabetti-circle.git
 
   cat > ~/.netrc << EOF
   machine api.heroku.com
